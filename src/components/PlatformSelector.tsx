54fa26f384a66bcae871334fe8b1c3a06fd1a85c
#Elzarha <s.seang.sony@gmail.com>
2024-05-24 14:15:31 +0700

    Refactor- simplify game query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,27 +1,32 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
-import { BsChevronDown } from "react-icons/bs";
-import usePlatforms , { Platform } from "../hooks/usePlatforms";
+import {Button, Menu, MenuButton, MenuItem, MenuList} from "@chakra-ui/react";
+import {BsChevronDown} from "react-icons/bs";
+import usePlatforms, {Platform} from "../hooks/usePlatforms";
 
-export interface Props { 
-  onSelectPlatform: (platform: Platform) => void;
-  selectedPlatform: Platform | null;
+export interface Props {
+    onSelectPlatform: (platform: Platform) => void;
+    selectedPlatId?: number;
 }
 
-const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
-  const { data, error } = usePlatforms();
+const PlatformSelector = ({
+                              onSelectPlatform,
+                              selectedPlatId
+                          }: Props) => {
+    const {data, error} = usePlatforms();
+    const selectedPlatform = data?.results.find(p => p.id === selectedPlatId)
 
-  if (error) return null;
-  
-  return (
-    <Menu>
-      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || 'Platforms'}
-      </MenuButton>
-      <MenuList>
-        {data?.results.map(platform => <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
-      </MenuList>
-    </Menu>
-  );
+    if (error) return null;
+
+    return (
+        <Menu>
+            <MenuButton as={Button} rightIcon={<BsChevronDown/>}>
+                {selectedPlatId?.name || 'Platforms'}
+            </MenuButton>
+            <MenuList>
+                {data?.results.map(platform => <MenuItem onClick={() => onSelectPlatform(platform)}
+                                                         key={platform.id}>{platform.name}</MenuItem>)}
+            </MenuList>
+        </Menu>
+    );
 };
 
 export default PlatformSelector;
